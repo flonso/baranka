8ea0148ceb2c78cdbf6946539aea5c6bd7bbbb54
Florian Alonso <5489296+flonso@users.noreply.github.com>
2019-09-05 15:56:23 +0200

    Added jquery ui

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-
 @extends('layouts.app')
 
 @section('title', 'Jeu')
@@ -11,34 +9,35 @@
 @section('content')
 	<div class="row">
 		<div class="col">
-			<button type="button" class="btn btn-block">
+			<button type="button" class="btn btn-block btn-primary">
 				Entrer les points d'une partie de Momand'lou
 			</button>
 		</div>
 		<div class="col">
-			<button type="button" class="btn btn-block">
+			<button type="button" class="btn btn-block btn-primary">
 				Enregistrer un certificat de découverte
 			</button>
 		</div>
 		<div class="col">
-			<button type="button" class="btn btn-block">
+			<button type="button" class="btn btn-block btn-primary">
 				Valider une quête
 			</button>
 		</div>
 		<div class="col">
-			<button type="button" class="btn btn-block">
-				Augmenter le niveau d'un jour
+			<button type="button" class="btn btn-block btn-primary">
+				Augmenter le niveau d'un joueur
 			</button>
 		</div>
 	</div>
+
 	<div class="row">
 		<div class="col">
-			<button type="button" class="btn btn-block">
+			<button type="button" class="btn btn-block btn-primary">
 				Enregistrer une pièce de bateau
 			</button>
 		</div>
 		<div class="col">
-			<button type="button" class="btn btn-block">
+			<button type="button" class="btn btn-block btn-primary">
 				Enregistrer un certificat d'acheminement
 			</button>
 		</div>
@@ -47,22 +46,25 @@
 		<div class="col">
 		</div>
 	</div>
+
 	<div class="row">
 		<div class="col">
-			<button type="button" class="btn btn-block">
+			<button type="button" class="btn btn-block btn-primary">
 				Enregistrer un bonus
 			</button>
 		</div>
 		<div class="col">
-			<button type="button" class="btn btn-block">
+			<button type="button" class="btn btn-block btn-primary">
 				Enregistrer un malus
 			</button>
 		</div>
 		<div class="col">
-			<button type="button" class="btn btn-block">
+			<button type="button" class="btn btn-block btn-primary">
 				Enregistrer la découverte du trésor!
 			</button>
 		</div>
+		<div class="col">
+		</div>
 	</div>
-	
+
 @endsection
